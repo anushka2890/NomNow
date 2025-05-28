226cfe97d67f16230b15c9a6cdb8b7dccd9139a7
anushka2890 <anushka28903@gmail.com>
2025-05-29 00:24:40 +0530

    adding service layer

---
+++ restaurant-service/src/main/java/com/nom/restaurant_service/service/MenuItemService.java
@@ -0,0 +1,35 @@
+package com.nom.restaurant_service.service;
+
+import com.nom.restaurant_service.model.MenuItem;
+import com.nom.restaurant_service.model.Restaurant;
+import com.nom.restaurant_service.repository.MenuItemRepository;
+import com.nom.restaurant_service.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    @Autowired
+    private MenuItemRepository menuItemRepository;
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public MenuItem addMenuItem(Long restaurantId, MenuItem menuItem) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new RuntimeException("Restaurant not found with id " + restaurantId));
+        menuItem.setRestaurant(restaurant);
+        return menuItemRepository.save(menuItem);
+    }
+
+    public List<MenuItem> getMenuItemsByRestaurant(Long restaurantId) {
+        return menuItemRepository.findByRestaurantId(restaurantId);
+    }
+
+    public void deleteMenuItem(Long id) {
+        menuItemRepository.deleteById(id);
+    }
+}
---
+++ restaurant-service/src/main/java/com/nom/restaurant_service/service/RestaurantService.java
@@ -0,0 +1,47 @@
+package com.nom.restaurant_service.service;
+
+import com.nom.restaurant_service.model.MenuItem;
+import com.nom.restaurant_service.model.Restaurant;
+import com.nom.restaurant_service.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RestaurantService {
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public Restaurant createRestaurant(Restaurant restaurant) {
+        return restaurantRepository.save(restaurant);
+    }
+
+    public Restaurant findRestaurantById(Long id) {
+        return restaurantRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Restaurant not found with id " + id)
+        );
+    }
+
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    public void deleteRestaurant(Long id) {
+        restaurantRepository.deleteById(id);
+    }
+
+    public Restaurant updateRestaurant(Long id, Restaurant updatedRestaurant) {
+        return restaurantRepository.findById(id).map(existingRestaurant -> {
+            existingRestaurant.setName(updatedRestaurant.getName());
+            existingRestaurant.setAddress(updatedRestaurant.getAddress());
+            for (MenuItem item : updatedRestaurant.getMenuItems()) {
+                item.setRestaurant(existingRestaurant); // maintain bidirectional link
+                existingRestaurant.getMenuItems().add(item);
+            }
+            existingRestaurant.setRating(updatedRestaurant.getRating());
+            return restaurantRepository.save(existingRestaurant);
+        }).orElseThrow(() -> new RuntimeException("Restaurant not found with id: " + id));
+    }
+
+}
