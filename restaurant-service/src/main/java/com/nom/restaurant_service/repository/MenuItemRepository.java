c2650740cf56327a2c79e7686d7bc0017be41749
anushka2890 <anushka28903@gmail.com>
2025-05-28 23:48:02 +0530

    adding repository

---
+++ restaurant-service/src/main/java/com/nom/restaurant_service/repository/MenuItemRepository.java
@@ -0,0 +1,9 @@
+package com.nom.restaurant_service.repository;
+
+import com.nom.restaurant_service.model.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+}
