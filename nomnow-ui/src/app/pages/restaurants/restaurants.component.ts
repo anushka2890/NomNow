659d4937cbc0f97bdb836aef26adeb71c1ec5e95
anushka2890 <anushka28903@gmail.com>
2025-06-08 17:35:36 +0530

    updating the restaurant ui

--- nomnow-ui/src/app/pages/restaurants/restaurants.component.ts
+++ nomnow-ui/src/app/pages/restaurants/restaurants.component.ts
@@ -1,27 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantService } from '../../services/restaurant.service';
-import { Restaurant } from '../../models/restaurant';
-
+import { Restaurant, MenuItem } from '../../models/restaurant';
+import { MenuModalComponent } from '../../shared/menu-modal/menu-modal.component';
 import { HttpClientModule } from '@angular/common/http'; //
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-restaurants',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, MenuModalComponent, FormsModule],
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
   restaurants: Restaurant[] = [];
+  selectedMenu: MenuItem[] = [];
+  selectedRestaurant: string = '';
+  showModal = false;
+  loading = true;
+  searchQuery : string = '';
+  filteredRestaurants: Restaurant[] = [];
 
   constructor(private restaurantService: RestaurantService) {}
 
   ngOnInit(): void {
       this.restaurantService.getRestaurants().subscribe({
-        next: (data: Restaurant[]) => {
+        next: (data) => {
           this.restaurants = data;
+          this.filteredRestaurants = data;
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error('Error fetching restaurants:', error);
+          this.loading = false;
         }
       });
     }
+
+    filterRestaurants() {
+      const query = this.searchQuery.toLowerCase().trim();
+      this.filteredRestaurants = this.restaurants.filter(r =>
+        r.name.toLowerCase().includes(query)
+      );
+    }
+
+    viewMenu(restaurant: Restaurant) {
+      this.selectedMenu = restaurant.menuItems;
+      this.selectedRestaurant = restaurant.name;
+      this.showModal = true;
+    }
+
+    closeModal = () => {
+      this.showModal = false;
+    };
 }
