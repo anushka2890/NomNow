cd8cc63abf632842f5d361a9688b490ed514ea24
anushka2890 <anushka28903@gmail.com>
2025-06-08 17:36:03 +0530

    adding router module to navbar

--- nomnow-ui/src/app/shared/navbar/navbar.component.ts
+++ nomnow-ui/src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']  // <-- note "styleUrls" (not "styleUrl")
+  styleUrls: ['./navbar.component.css'],  // <-- note "styleUrls" (not "styleUrl")
+  imports:[RouterModule]
 })
 export class NavbarComponent {}
