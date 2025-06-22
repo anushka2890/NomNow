3fb29856b51426d90dbbff8ab9d5036c77f30c0f
anushka2890 <anushka28903@gmail.com>
2025-06-22 21:14:59 +0530

    adding navbar and hero component to home page

--- nomnow-ui/src/app/app.component.ts
+++ nomnow-ui/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HeroComponent } from "./sections/hero/hero.component";
 @Component({
   selector: 'app-root',
   standalone: true, // this is the trick: this component is standalone
-  imports: [RouterModule, HttpClientModule, HeaderComponent, MatSnackBarModule],
+  imports: [RouterModule, HttpClientModule, HeaderComponent, MatSnackBarModule, NavbarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
