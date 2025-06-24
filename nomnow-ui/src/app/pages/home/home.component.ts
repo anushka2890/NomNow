ad1b7f06489a1599986ce0271e745c611d317ce5
anushka2890 <anushka28903@gmail.com>
2025-06-24 18:22:20 +0530

    correcting home-component

--- nomnow-ui/src/app/pages/home/home.component.ts
+++ nomnow-ui/src/app/pages/home/home.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
+import { HeroComponent } from "../../sections/hero/hero.component";
+import { CategoryStripComponent } from "../../sections/category-strip/category-strip.component";
+import { FeaturedRestaurantsComponent } from "../../sections/featured-restaurants/featured-restaurants.component";
+import { ExclusiveDealsComponent } from "../../sections/exclusive-deals/exclusive-deals.component";
+import { HowItWorksComponent } from "../../sections/how-it-works/how-it-works.component";
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HeroComponent, CategoryStripComponent, FeaturedRestaurantsComponent, ExclusiveDealsComponent, HowItWorksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
