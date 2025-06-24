ed699921629f990cb82fcf0a4339ec43b1ca7175
anushka2890 <anushka28903@gmail.com>
2025-06-24 18:21:53 +0530

    create enriched-order-response.model

---
+++ nomnow-ui/src/app/models/enriched-order-response.model.ts
@@ -0,0 +1,12 @@
+export interface EnrichedOrderItem {
+  name: string;
+  quantity: number;
+}
+
+export interface EnrichedOrder {
+  orderId: number;
+  status: string;
+  items: EnrichedOrderItem[];
+  restaurantName: string;
+  orderTime: string;
+}
