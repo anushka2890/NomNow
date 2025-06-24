c12448338a565e7e5ff2bf10afdbea55e10fe091
anushka2890 <anushka28903@gmail.com>
2025-06-24 18:25:04 +0530

    update order-status

--- nomnow-ui/src/app/pages/order-status/order-status.component.ts
+++ nomnow-ui/src/app/pages/order-status/order-status.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Subscription, interval } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { OrderResponse } from '../../models/OrderResponse.model';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-order-status',
@@ -18,7 +20,7 @@ export class OrderStatusComponent implements OnInit, OnDestroy{
   order?: OrderResponse;
   pollingSubscription?: Subscription;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute ) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute, private auth: AuthService ) {}
 
   ngOnInit(): void {
     this.orderId = Number(this.route.snapshot.paramMap.get('id'));
@@ -26,8 +28,13 @@ export class OrderStatusComponent implements OnInit, OnDestroy{
        console.error('Order ID route param is missing or invalid');
        return;
      }
+     const token = this.auth.getToken();
+     const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    });
     this.pollingSubscription = interval(5000).subscribe(() => {
-      this.http.get<OrderResponse>(`http://localhost:8081/api/orders/${this.orderId}`)
+      this.http.get<OrderResponse>(`${environment.apiUrl}/orders/${this.orderId}`, { headers })
       .subscribe(data => {
         this.order = data;
         if(['PAYMENT_SUCCESS', 'PAYMENT_FAILED'].includes(this.order.status)){
