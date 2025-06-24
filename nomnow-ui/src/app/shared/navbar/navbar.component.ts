d7e2a05e8bf7ca07b4cebac8f7382ff82ed28319
anushka2890 <anushka28903@gmail.com>
2025-06-24 18:16:07 +0530

    updating the navbar

--- nomnow-ui/src/app/shared/navbar/navbar.component.ts
+++ nomnow-ui/src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginModalComponent } from '../../auth/login-modal/login-modal.component';
+import { CartUiService } from '../../services/cart-ui.service';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Observable, take } from 'rxjs';
+import { ProfileSidebarComponent } from "../profile-sidebar/profile-sidebar.component";
+import { User, UserService } from '../../services/user.service';
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],  // <-- note "styleUrls" (not "styleUrl")
-  imports:[RouterModule]
+  imports: [RouterModule, CommonModule, ProfileSidebarComponent]
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit{
+  isLoggedIn$!: Observable<boolean>;
+  showProfileSidebar = false;
+  user?: User;
+
+  constructor(private dialog: MatDialog
+    , private cartUiService: CartUiService
+    , private authService: AuthService
+    , private userService: UserService
+    ,  private router: Router) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+    // ✅ Fetch user only if logged in
+    this.authService.isLoggedIn$.subscribe((loggedIn) => {
+      if (loggedIn) {
+        this.userService.getUserProfile().subscribe({
+          next: (data) => this.user = data,
+          error: (err) => console.error('Failed to fetch user info', err)
+        });
+      } else {
+        this.user = undefined;
+      }
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  openLogin(): void {
+    this.dialog.open(LoginModalComponent, {
+      width: '400px'
+    });
+  }
+  openCart() {
+    this.cartUiService.open();
+  }
+
+  toggleProfileSidebar(): void {
+    console.log('Avatar clicked, sidebar toggled');
+    this.showProfileSidebar = !this.showProfileSidebar;
+  }
+
+  trackLastOrder() {
+  this.authService.isLoggedIn$.pipe(take(1)).subscribe(loggedIn => {
+    if (!loggedIn) {
+      alert('Please log in to track your orders.');
+      return;
+    }
+
+    const orderId = localStorage.getItem('lastOrderId');
+    if (orderId) {
+      this.router.navigate(['/order-status', orderId]);
+    } else {
+      alert('No recent order found.');
+    }
+  });
+}
+
+}
