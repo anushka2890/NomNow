2a91dd42cf8381272b2b3b42bd804251bf1eac22
anushka2890 <anushka28903@gmail.com>
2025-06-24 18:22:03 +0530

    adding dto

--- nomnow-ui/src/app/models/restaurant.ts
+++ nomnow-ui/src/app/models/restaurant.ts
@@ -3,10 +3,29 @@ export interface Restaurant {
   name: string;
   address: string;
   rating: number;
+  imageUrl: string;
   menuItems: MenuItem[];
 }
 
 export interface MenuItem {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  availableQuantity: number;
+  imageUrl: string;
+  category: string;
+}
+
+export interface RestaurantDTO {
+  id: number;
+  name: string;
+  address: string;
+  rating: number;
+  imageUrl: string;
+}
+
+export interface MenuItemDTO {
   id: number;
   name: string;
   description: string;
